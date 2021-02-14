052d9cb581abca26353462ec0cd9c16457988e00
Xavier T``` <xavier-tan@hotmail.sg>
2021-02-13 16:10:22 -0800

    done shop?

--- components/product-item.js
+++ components/product-item.js
@@ -2,6 +2,114 @@
 
 class ProductItem extends HTMLElement {
   // TODO
+  constructor() {
+    // call superclass' constructor first
+    super();
+
+    // elem functionality here:
+    // attaching shadow root
+    let shadow = this.attachShadow({mode: 'open'});
+
+    // create internal shadow DOM structure + style
+    // and attach both to shadow root at the end
+
+    // create nested elements
+    let product = document.createElement('li');
+    product.setAttribute('class', 'product');
+
+    let img = product.appendChild(document.createElement('img'));
+    img.setAttribute('src', 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg');
+    img.setAttribute('alt', 'Fjallraven - Foldstack No. 1 Backpack, Fits 15 Laptops');
+    img.setAttribute('width', 200);
+    // img.src = this.hasAttribute('img') ? this.getAttribute('img') : '';
+
+    let title = product.appendChild(document.createElement('p'));
+    title.setAttribute('class', 'title');
+    // title.textContent = "Fjallraven";
+
+    let price = product.appendChild(document.createElement('p'));
+    price.setAttribute('class', 'price');
+
+    let btn = product.appendChild(document.createElement('button'));
+    btn.setAttribute('onclick', "alert('Added to Cart!')");
+    btn.textContent = "Add to Cart";
+
+    // Create some CSS to apply to the shadow dom
+    const style = document.createElement('style');
+    // console.log(style.isConnected);
+
+    style.textContent = `
+      .price {
+        color: green;
+        font-size: 1.8em;
+        font-weight: bold;
+        margin: 0;
+      }
+
+      .product {
+        align-items: center;
+        background-color: white;
+        border-radius: 5px;
+        display: grid;
+        grid-template-areas:
+        'image'
+        'title'
+        'price'
+        'add';
+        grid-template-rows: 67% 11% 11% 11%;
+        height: 450px;
+        filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+        margin: 0 30px 30px 0;
+        padding: 10px 20px;
+        width: 200px;
+      }
+
+      .product > button {
+        background-color: rgb(255, 208, 0);
+        border: none;
+        border-radius: 5px;
+        color: black;
+        justify-self: center;
+        max-height: 35px;
+        width: 180px;
+        padding: 8px 20px;
+        transition: 0.1s ease all;
+      }
+
+      .product > button:hover {
+        background-color: rgb(255, 166, 0);
+        cursor: pointer;
+        transition: 0.1s ease all;
+      }
+
+      .product > img {
+        align-self: center;
+        justify-self: center;
+        width: 100%;
+      }
+
+      .title {
+        font-size: 1.1em;
+        margin: 0;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+      }
+
+      .title:hover {
+        font-size: 1.1em;
+        margin: 0;
+        white-space: wrap;
+        overflow: auto;
+        text-overflow: unset;
+      }
+      `;
+    // attach the created elements to the shadow DOM
+    // this.shadowRoot.append(style,wrapper);
+    console.log(style.isConnected);
+    shadow.appendChild(product);
+    shadow.appendChild(style);
+  }
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,78 @@
 // Script.js
+storage = window.localStorage;
 
+// create an array to store product id's
+let boughtItems = [];
+
+// fetch data and check if need to store into storage
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
+  if (!storage.getItem("json obj")) {
+    fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(data => storage.setItem('json obj', JSON.stringify(data)));
+  }
+  if (! storage.getItem('boughtItems')) {
+    storage.setItem('boughtItems', boughtItems);
+  }
 });
+
+// set aesthetics of website elements:
+// let cartIcon = document.getElementById("cart-icon")
+// let cartImg = cartIcon.appendChild(document.createElement('img'));
+// cartImg.setAttribute('src', 'assets/cart-icon.png');
+//
+// let shopIcon = document.getElementById("shop-icon")
+// let shopImg = shopIcon.appendChild(document.createElement('img'));
+// shopImg.setAttribute('src', 'assets/shop-icon.png');
+
+// prepare product list
+let prodList = document.getElementById("product-list");
+let searchableProdList = [];
+let prods = JSON.parse(storage.getItem('json obj'));
+
+for (let i = 0; i < prods.length; i++) {
+  let item = document.createElement('product-item');
+  // Set all the attributes for each product-item
+  item.shadowRoot.childNodes[0].childNodes[0].setAttribute('src', prods[i]['image']);
+  item.shadowRoot.childNodes[0].childNodes[0].setAttribute('alt', prods[i]['description']);
+  item.shadowRoot.childNodes[0].childNodes[1].textContent = prods[i]['title'];
+  item.shadowRoot.childNodes[0].childNodes[2].textContent = prods[i]['price'];
+
+  // add an event listener for everytime the item's add or remove cart btn is clicked
+  item.shadowRoot.childNodes[0].childNodes[3].addEventListener('click', (e) => {
+    if (item.shadowRoot.childNodes[0].childNodes[3].textContent == "Add to Cart") {
+      boughtItems.push(prods[i]['id']);
+      storage.setItem('boughtItems', boughtItems);
+      item.shadowRoot.childNodes[0].childNodes[3].textContent = "Remove from Cart";
+      item.shadowRoot.childNodes[0].childNodes[3].setAttribute('onclick', "alert('Removed from Cart!')");
+      document.getElementById('cart-count').textContent = Number(document.getElementById('cart-count').textContent) + 1;
+    }
+    else if (item.shadowRoot.childNodes[0].childNodes[3].textContent == "Remove from Cart") {
+      // get index of this item, splice it out
+      let idx = boughtItems.indexOf(prods[i]['id']);
+      if (idx > -1) {
+        boughtItems.splice(idx, 1);
+      }
+      // update localStorage
+      storage.setItem('boughtItems', boughtItems);
+      // change add to cart button
+      item.shadowRoot.childNodes[0].childNodes[3].textContent = "Add to Cart";
+      item.shadowRoot.childNodes[0].childNodes[3].setAttribute('onclick', "alert('Added to Cart!')");
+      document.getElementById('cart-count').textContent = Number(document.getElementById('cart-count').textContent) - 1;
+    }
+    // this.textContent = "Remove From Cart";
+  });
+  prodList.appendChild(item);
+  searchableProdList.push(item);
+}
+
+// retrieve stored items into boughtItems array
+boughtItems = JSON.parse("[" + storage.getItem('boughtItems') + "]");
+
+// update the cart for all bought items + button states
+for (let j = 0; j < boughtItems.length; j++) {
+    searchableProdList[boughtItems[j]].shadowRoot.childNodes[0].childNodes[3].textContent = "Remove from Cart";
+    document.getElementById('cart-count').textContent = Number(document.getElementById('cart-count').textContent) + 1;
+    searchableProdList[boughtItems[j]].shadowRoot.childNodes[0].childNodes[3].setAttribute('onclick', "alert('Removed from Cart!')");
+}
